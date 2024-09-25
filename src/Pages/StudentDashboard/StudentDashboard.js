b108d5782013e63b3a1766b073314f188aa84d2a
Pranshu Singh <ethird02@gmail.com>
2024-09-25 13:32:28 +0530

    update2

---
+++ src/Pages/StudentDashboard/StudentDashboard.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './StudentDashboard.css'
+
+const StudentDashboard=()=>{
+    return(
+        <div>StudentDashboard unver process</div>
+    );
+}
+
+export default StudentDashboard
